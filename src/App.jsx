fa027df8f8c85d205062a79d5a8647316f4a3162
Saireddy0139 <saireddy0072002@gmail.com>
2024-03-04 17:22:29 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -7,44 +7,45 @@ import '@aws-amplify/ui-react/styles.css';
 import { Auth } from "aws-amplify";
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
-// function App() {
-//   const {signOut}= useAuthenticator();
+function App() {
+  const {user ,signOut}= useAuthenticator();
  
  
-//   return (
-   
-   
-//     <nav className="navbar bg-dark">
-//     <div className="container-fluid">
-//         <span className="appName">
-//             CloudXcel User Authentication</span>
-//             <button  onClick={signOut}>Sign out</button>
-//     </div>
-// </nav>
-    
-    
-
-//   );
-// }
-
-// export default withAuthenticator(App);
-function App({ user, signOut }) {
   return (
-    <div>
-      <div className="flex justify-end px-4 py-2">
-        <button
-          type="button"
-          className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white"
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
-      <div className="flex justify-center items-center h-screen w-full">
-        Hello World
-      </div>
+   
+   
+    <nav className="navbar bg-dark">
+    <div className="container-fluid">
+        <span className="appName">
+            CloudXcel User Authentication {user.username}</span>
+            <button  onClick={signOut}>Sign out</button>
     </div>
+</nav>
+    
+    
+
   );
 }
 
 export default withAuthenticator(App);
+// function App({ user, signOut }) {
+//   return (
+//     <div>
+//       <div className="flex justify-end px-4 py-2">
+//         <div className="mr-4">Welcome, {user.username}</div>
+//         <button
+//           type="button"
+//           className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white"
+//           onClick={() => signOut()}
+//         >
+//           Sign Out
+//         </button>
+//       </div>
+//       <div className="flex justify-center items-center h-screen w-full">
+//         Hello World
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
