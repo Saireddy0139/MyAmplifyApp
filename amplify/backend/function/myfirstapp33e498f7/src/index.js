9396ba0921af7aa7f7dab93acbf719eadae49aa0
Saireddy0139 <saireddy0072002@gmail.com>
2024-03-05 16:05:12 +0530

    new

--- amplify/backend/function/myfirstapp33e498f7/src/index.js
+++ amplify/backend/function/myfirstapp33e498f7/src/index.js
@@ -71,14 +71,14 @@ export async function handler(event, context) {
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    ...response,
+    
     
     headers: {
          "Access-Control-Allow-Origin": "*",
          "Access-Control-Allow-Headers": "*"
      },
-     body: JSON.stringify(response.body),
-        // body: JSON.stringify('Hello from Lambda!'),
+     //body: JSON.stringify(response.body),
+        body: JSON.stringify('Hello from Lambda!'),
     };
 };
   // return {
