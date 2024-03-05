1c9d39f674dde7d2a129e6c461c0ec148841c15a
Saireddy0139 <saireddy0072002@gmail.com>
2024-03-05 16:03:46 +0530

    new

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -47,5 +47,48 @@ export const deleteUserDetails = /* GraphQL */ `
       updatedAt
       __typename
     }
-  }
-`;
+ }
+  `;
+// import { generateClient } from "aws-amplify/api";
+// import { createUserdetasils } from './graphql/mutations';
+
+
+// import { generateClient } from "aws-amplify/api";
+// import { updateUserdetasils } from './graphql/mutations';
+
+// // const client = generateClient()
+
+
+// import { generateClient } from "aws-amplify/api";
+// import { deleteUserdetasils } from './graphql/mutations';
+
+// // const client = generateClient()
+// const client = generateClient()
+// const newUserdetasils = await client.graphql({
+//   query: createUserdetasils,
+//   variables: {
+//       input: {
+//   "email": "Lorem ipsum dolor sit amet",
+//   "username": "Lorem ipsum dolor sit amet",
+//   "password": "Lorem ipsum dolor sit amet"
+// }
+//   }
+// });
+// const updatedUserdetasils = await client.graphql({
+//   query: updateUserdetasils,
+//   variables: {
+//       input: {
+//   "email": "Lorem ipsum dolor sit amet",
+//   "username": "Lorem ipsum dolor sit amet",
+//   "password": "Lorem ipsum dolor sit amet"
+// }
+//   }
+// });
+// const deletedUserdetasils = await client.graphql({
+//   query: deleteUserdetasils,
+//   variables: {
+//       input: {
+//           id: "YOUR_RECORD_ID"
+//       }
+//   }
+// });
